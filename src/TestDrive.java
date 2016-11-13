c3bd71150b661692df6db1165b31bd4df1bd048d
CassieAin <roksolana.d22@gmail.com>
2016-11-13 17:49:08 +0200

    Main class source code

---
+++ src/TestDrive.java
@@ -0,0 +1,15 @@
+import Facade.GalleryFacade;
+
+/**
+ * Test Drive class for using Facade
+ * with main
+ * @author Dyachuk Roksolana
+ *
+ */
+public class TestDrive {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GalleryFacade facade = new GalleryFacade();
+		facade.chooseOccupation();
+	}
+} 
